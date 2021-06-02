e22d283fc21ec0198952314525f36e4926b012b9
Robert Andrei <robert_amarandei@yahoo.com>
2021-06-02 09:47:59 +0300

    Add logo

--- src/components/StyledBanner.tsx
+++ src/components/StyledBanner.tsx
@@ -1,7 +1,32 @@
+import { Box, makeStyles, Paper } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+  background: {
+    position: "relative",
+  },
+  logo: {
+    position: "absolute",
+    top: 8,
+    left: 14,
+    maxWidth: "50%",
+    minWidth: "20%",
+  },
+});
+
 const StyledBanner = () => {
-  return <img src="./assets/LoginBanner.png" height="auto" width="100%" />;
+  const classes = useStyles();
+  return (
+    <Box>
+      <img
+        src="./assets/LoginBanner.png"
+        height="auto"
+        width="100%"
+        className={classes.background}
+      />
+      <img src="./assets/LogoViasat.svg" className={classes.logo} />
+    </Box>
+  );
 };
 
 export default StyledBanner;
